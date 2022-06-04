5debd874a6dceebf2e9c9d6201d43e380e41b0da
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-06-04 14:36:58 +0530

    Update search.java

--- Array2/search.java
+++ Array2/search.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
 class search{
     public static void main(String[] args){
-        int [] arr={{1,3,5,8},{4,6,12,14}}
+        int [][] arr={{1,3,5,8},{4,6,12,14},{5,7,13,15},{6,8,14,16}};
+        System.out.println(search(arr,7));
+    }
+    public static boolean search(int[][] arr,int ele){
+        int row=0;
+        int col=arr[0].length-1;
+        while(col>=0&&row<arr.length)
+        {
+            if(arr[row][col]==ele){
+                return true;
+            }
+            else if(arr[row][col]>ele){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
     }
 }
