a3bb0abe7aed0db48db9c0f9bf81680dff8dc352
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-06-05 12:30:40 +0530

    Add files via upload

--- Array2/digonalTraversal.java
+++ Array2/digonalTraversal.java
@@ -1,12 +1,11 @@
-package Lec12;
+
 
 import java.util.ArrayList;
 import java.util.Collections;
-
-public class DiagonalTraversal {
+ class digonalTraversal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 		DT(arr);
 
