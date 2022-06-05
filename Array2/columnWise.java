e1e8c3c344c3be297de8f859cdea83d87dcd6490
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-06-05 12:30:19 +0530

    Add files via upload

--- Array2/columnWise.java
+++ Array2/columnWise.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  class columnWise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 		
